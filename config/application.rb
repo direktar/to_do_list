be559c0c86daf94b8a78fa7831bfce35eea2d9c8
direktar <p.zendran@gmail.com>
2020-04-28 14:18:34 +0300

    initialize_on_precompile set to false config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module ToDoList
     config.sass.preferred_syntax = :sass
     config.sass.line_comments = false
     config.sass.cache = false
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
